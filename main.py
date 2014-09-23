905da46ad1994652e01fd6f88528e2509d46c25d
Joshua Cox <joshua.n.cox@gmail.com>
2014-09-22 21:08:22 -0700

    sprite directionality implemented

--- main.py
+++ main.py
@@ -29,160 +29,24 @@ from kivy.uix.image import Image
 from kivy.core.audio import SoundLoader
 from kivy.core.window import Window
 from kivy.core.image import Image as CImage
+from kivy.animation import Animation
 
-import xml.etree.ElementTree as ET
 import time
 import random
 
-class TileManager():
-    '''take in XML sheet, and produce <map>, <tileSet>, and <layerTiles>
-    '''
-    def __init__(self, field, atlas):
-        self.field = field
-        self.source = ''
-        self.sourceWidth = 0
-        self.sourceHeight = 0
-        self.tileHeight = 0
-        self.tileWidth = 0
-        self.firstgid = 0
-        
-        # layer width height == mapWidth mapHeight
-        self.layer = '' 
-        self.gid = 0
-        self.tileSet = {}
-        self.layerTiles = {}
-        self.main(atlas)
-        
-        self.send(self.field, self.tileMap,self.tileSet,self.layerTiles)
-            
-    def main(self, atlas):
-        tree = ET.parse(atlas)
-        root = tree.getroot()
-        
-        self.mapWidth = int(root.attrib['width'])
-        self.mapHeight = int(root.attrib["height"])
-        self.mapTileWidth = int(root.attrib["tilewidth"])
-        self.mapTileHeight = int(root.attrib["tileheight"])
-        
-        self.tileMap = dict(
-                        mapWidth = self.mapWidth,
-                        mapHeight = self.mapHeight,
-                        mapTileWidth = self.mapTileWidth,
-                        mapTileHeight = self.mapTileHeight
-                        )
-        
-        for child in root:
-            
-            if child.tag == 'tileset':
-                self.tileName = child.attrib['name']
-                self.tileHeight = int(child.attrib['tileheight'])
-                self.tileWidth = int(child.attrib['tilewidth'])
-                self.firstgid = int(child.attrib['firstgid'])
-                for image in child:
-                    self.source = image.attrib['source']
-                    self.sourceWidth = int(image.attrib['width'])
-                    self.sourceHeight = int(image.attrib['height'])
-                    y = self.sourceHeight/self.tileHeight
-                    x = self.sourceWidth/self.tileWidth
-                    self.lastgid = x * y + self.firstgid - 1
-                    
-                self.tileSet[self.firstgid] = dict(
-                                                   source = "{}.png".format(self.tileName),
-                                                   sourceWidth = self.sourceWidth,
-                                                   sourceHeight = self.sourceHeight,
-                                                   tileHeight = self.tileHeight,
-                                                   tileWidth = self.tileWidth,
-                                                   firstgid = self.firstgid,
-                                                   lastgid = self.lastgid
-                                                   )
-            elif child.tag == 'layer':
-                self.layer = child.attrib['name']
-                self.layerTiles[self.layer] = [] # new list of tiles for each layer
-                
-                for index, tile in enumerate(child[0]): # skip <data> iter
-                    gid = int(tile.attrib['gid'])
-                    if gid > 0: # pass empty tiles
-                        self.layerTiles[self.layer].append({index:gid})
-                    else:
-                        pass
-                
-    def send(self, field, tileMap, tileSet, layerTiles):
-        print tileMap
-        print tileSet
-        print layerTiles
-        BuildTiles(field, tileMap, tileSet, layerTiles)
-
-class BuildTiles():
-    def __init__(self, field, tileMap, tileSet, layerTiles):  
-        self.field = field
-        self.tileMap = tileMap
-        self.tileSet = tileSet
-        self.layerTiles = layerTiles
-        self.loadTextures()
-        self.prepareTile()
+class WSprite(Widget):
+    pos = ListProperty()
+    def __init__(self,tex, **kwargs):
+        super(WSprite, self).__init__(**kwargs)
+        self.tex = tex
+        self.rect = Rectangle(texture = self.tex,
+                              size = self.size,
+                              pos = self.pos)
+        with self.canvas:
+            self.rect
     
-    def loadTextures(self):
-        self.textures = {}
-        for item in self.tileSet:
-            texture_name = self.tileSet[item]['firstgid']
-            texture_source = "resources/assets/{}".format(self.tileSet[item]['source'])
-            self.textures[texture_name] = CImage.load(texture_source, keep_data = True).texture
-            
-    def prepareTile(self):
-        self.mapWidth = self.tileMap['mapWidth']
-        self.mapHeight = self.tileMap['mapHeight']
-        self.mapTileWidth = self.tileMap['mapTileWidth']
-        self.mapTileHeight = self.tileMap['mapTileHeight']
-        
-        for tiles in self.layerTiles['Background']:
-            for index in tiles:
-                self.buildTile(1, index, tiles[index])
-        for tiles in self.layerTiles['Foreground']:
-            for index in tiles:
-                self.buildTile(2, index, tiles[index])
-        for tiles in self.layerTiles['Top']:    
-            for index in tiles:
-                self.buildTile(3, index, tiles[index])
-            
-    def buildTile(self, layer, location, gid):
-        # find correct tile set for each tile
-        for item in self.tileSet:
-            last = self.tileSet[item]['lastgid']
-            first = self.tileSet[item]['firstgid']
-            if first <= gid <= last:
-                tileSet = self.tileSet[item]
-                #print tileSet
-        tileWidth = tileSet['tileWidth']
-        tileHeight = tileSet['tileHeight']
-        tileUnitWidth = tileSet['sourceWidth']/tileSet['tileWidth']
-        tileUnitHeight = tileSet['sourceHeight']/tileSet['tileHeight']
-        # load tile texture for the tile set
-        texture = self.textures[tileSet['firstgid']]
-        
-        # find location on the map for the tile
-        pos_y = (location // self.mapWidth) * self.mapTileHeight
-        pos_x = (location * self.mapTileWidth) - ((location // self.mapWidth) * self.mapWidth * self.mapTileWidth)
-        pos_y_offset = (self.mapHeight * self.mapTileHeight) - pos_y # need to invert y position placement
-        
-        # find the tile in the tile set
-        
-        gid_offset = gid - 1
-        y = (gid_offset // tileUnitWidth) * tileHeight
-        x = (gid_offset * tileWidth) - ((gid_offset/tileUnitWidth)*tileSet['sourceWidth'])
-        y_offset = tileSet['sourceHeight'] - y
-        
-        size = (tileWidth, tileHeight)
-        tile = texture.get_region(x,y_offset,64,64)
-        
-        # place the tile
-        if layer < 2:
-            print pos_x, pos_y, gid_offset, x, y_offset, location
-            with self.field.canvas.before:
-                Rectangle(texture=tile, size=size, pos = (pos_x,pos_y))
-        elif layer > 2:
-            pass
-            #with self.field.canvas:
-            #    Rectangle(texture=tile, size=size, pos = (pos_x,pos_y_offset)) 
+    def on_pos(self, instance, value):
+        print 'moved'
                    
 class AGSprite(Image):
     player = BooleanProperty(False)
@@ -195,7 +59,11 @@ class AGSprite(Image):
         super(AGSprite, self).__init__(**kwargs)
         self.size_hint=(None,None)
         self.size = size
-        
+        self.allow_stretch = True
+    
+    def on_start(self):
+        print 'i started' 
+           
 class AGBoundary(AGSprite):
     boundary = BooleanProperty(True)
     opacity = NumericProperty(0)
@@ -222,10 +90,7 @@ class AGScreenManager(ScreenManager):
         self.current = 'world'
     
     def moveField(self, direction):
-        self.world.moveField(direction)
-    
-    def toggleBoundaries(self):
-        self.world.toggleBoundaries()
+        pass
         
 class IntroScreen(Screen):
     def __init__(self, **kwargs):
@@ -266,7 +131,6 @@ class IntroScreen(Screen):
         self.parent.endIntro()
         
 class WorldScreen(Screen):
-    boundaries = BooleanProperty(False)
     move = NumericProperty(32)
     spriteList = ListProperty()
     player = ObjectProperty()
@@ -274,14 +138,12 @@ class WorldScreen(Screen):
     def __init__(self, **kwargs):
         super(WorldScreen, self).__init__(**kwargs)
         
-        atlas = 'resources/atlas/grass_field1.tmx'
-        
         field = self.buildField()
         self.buildPlayer()
-        self.buildSprites()
-        self.buildBoundaries()
-        
-        TileManager(field, atlas)
+        bg = self.loadBackground()
+        self.buildBackground(field, bg)
+        sprites = self.loadSprites()
+        #self.buildSprites(sprites)
         
         self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
         self._keyboard.bind(on_key_down=self._on_keyboard_down)
@@ -291,25 +153,46 @@ class WorldScreen(Screen):
         self._keyboard = None
     
     def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
-        self.moveField(keycode[1])
+        self.movePlayer(keycode[1])
         return True
-    
-    def moveField(self, direction):
-        #print 'moving field'
-        if direction == 'up':
-            self.field.y -= self.move
-            self.player.y += self.move
-        elif direction == 'down':
-            self.field.y += self.move
-            self.player.y -= self.move
-        elif direction == 'left':
-            self.field.x += self.move
-            self.player.x -= self.move
-        elif direction == 'right':
-            self.field.x -= self.move
-            self.player.x += self.move 
         
-        self.checkCollision(direction)    
+    def movePlayer(self, direction):
+        #print 'moving field'
+        y = self.player.y
+        x = self.player.x
+        
+        if direction == 'up':
+            y += self.move
+        elif direction == 'down':
+            y -= self.move
+        elif direction == 'left':
+            x -= self.move
+        elif direction == 'right':
+            x += self.move
+        
+        self.changePlayerImage(direction)
+        speed = 0.3
+        anim = Animation(x=x, y=y, d=speed)
+        anim.bind(on_start=self.startPlayerAnim, on_complete=self.stopPlayerAnim)
+        
+        anim.start(self.player)
+        self.checkCollision(direction) 
+           
+    def changePlayerImage(self, direction):
+        if direction == 'up':
+            self.player.source = 'resources/assets/Clyde_up.zip'
+        elif direction == 'down':
+            self.player.source = 'resources/assets/Clyde_down.zip'
+        elif direction == 'left':
+            self.player.source = 'resources/assets/Clyde_left.zip'
+        elif direction == 'right':
+            self.player.source = 'resources/assets/Clyde_right.zip'
+            
+    def startPlayerAnim(self, instance, value):
+        self.player.anim_delay=.1
+    
+    def stopPlayerAnim(self, instance, value):
+        self.player.anim_delay=-1
              
     def on_transition_state(self, instance, value):    
         sound = SoundLoader.load('resources/sound/overworld.wav')
@@ -325,96 +208,44 @@ class WorldScreen(Screen):
     def on_touch_down(self, value):
         print('value.pos: {} value.spos: {}'.format(value.pos, value.spos))
     
-    def loadTextures(self):
-        # include import of file for where tiles are located in sheet
-        
-        sheet = CImage.load('resources/tiles/basictheme.png', keep_data=True).texture
-        
-        x = 0
-        y = 0
-        size_x = 32
-        size_y = 32
-        
-        desert_1 = sheet.get_region(x,y,size_x,size_y)
-        
-        tiles = dict(desert_1 = desert_1)
-        return tiles
-        
-        #tileGrass = CImage.load('resources/tiles/basictheme/tile_0025_Layer-55.png', keep_data=True).texture
-        #return tileGrass
+    def loadBackground(self):
+        bg = CImage.load('resources/bg/grass_field1.png', keep_data=True).texture
+        return bg
     
+    def buildBackground(self, field, bg):
+        with field.canvas.before:
+            Rectangle(texture = bg, pos=(0,0), size=(640,640))
+            
     def buildField(self):
         self.field = RelativeLayout()
-        #with self.field.canvas.before:
-            #Rectangle(source ='resources/bg/village.png', center=(256,256), size=(2048,2048))
         self.add_widget(self.field)
         
         return self.field
-    
-    def buildTiles(self, tile, field):
-        # import list of where tiles will be placed
-        tileHeight = 32
-        tileWidth = 32
-        tileSize = (tileWidth,tileHeight)
-        x = 256
-        y = 256
-        #x_offset = x
-        #y_offset = mapHeight - y
-        
-        with field.canvas.before:
-            Rectangle(texture=tile['desert_1'], size=tileSize, pos = (256,256))
-        
-    def bkp_buildTiles(self, texture, field):
-        x=64
-        y=64
-        fieldWidth = 40 # multiply by 32px
-        fieldHeight = 40
-        size = (64,64)
-        clipbl = texture.get_region(0,0,16,16)
-        cliptl = texture.get_region(0,16,16,16)
-        clipbr = texture.get_region(16,0,16,16)
-        cliptr = texture.get_region(16,16,16,16)
-        
-        with field.canvas.before:
-            '''
-            Rectangle(texture=clipbl, size=(64,64), pos = (128,128))
-            Rectangle(texture=cliptl, size=(64,64), pos = (128,192))
-            Rectangle(texture=clipbr, size=(64,64), pos = (192,128))
-            Rectangle(texture=cliptr, size=(64,64), pos = (192,192))
-            Rectangle(texture=texture, size=size, pos = (256,128))
-            '''
-        
-        for ny in range(fieldHeight):
-            for nx in range(fieldWidth):
-                with field.canvas.before:
-                    Rectangle(texture=texture,size=size, pos = (nx*x,ny*y)) 
         
     def buildPlayer(self):
-        self.player = AGSprite(player = True, source = 'resources/icons/bunnyfront1.png', size=(62,62), center=(513,257))
-        self.field.add_widget(self.player)
-            
-    def buildSprites(self):
-        block = AGSprite(solid=True,size=(64,64), source='resources/icons/branches.png', center = (512,512))
-        self.field.add_widget(block)
-        self.spriteList.append(block)
+        self.player = AGSprite(source = 'resources/assets/Clyde_down.zip', size=(30,30), center=(513,257))
+        self.player.anim_delay=-1
+        self.field.add_widget(self.player)   
     
-    def buildBoundaries(self):
-        pass
+    def loadSprites(self):
+        bush1 = 'resources/assets/qubodup-bush_0.png'    
+        cat = 'resources/assets/giphy.gif'
+        town = 'resources/icons/buddhist.png'
         
-    def toggleBoundaries(self):
-        if self.boundaries:
-            self.boundaries=False
-        else:
-            self.boundaries=True
-            
-    def on_boundaries(self, instance, value):
-        for child in self.field.children:
-            if value:
-                if child.boundary:
-                    child.opacity = 0.8
-            else:
-                if child.boundary:
-                    child.opacity = 0
+        sprites = dict(bush1 = bush1,
+                       cat = cat,
+                       town = town
+                       )
+        return sprites
+       
+    def buildSprites(self, sprites):
+        
+        block = AGSprite(solid=True, size=(32,32), source=sprites['bush1'], center = (512,512))
+        town1 = AGSprite(town = True, size=(96,96), source=sprites['town'], center = (256,512))
+        self.field.add_widget(block)
+        self.field.add_widget(town1)
+        self.spriteList.append(block)
+        self.spriteList.append(town1)
     
     def checkCollision(self, direction):
         for child in self.spriteList:
@@ -435,18 +266,19 @@ class WorldScreen(Screen):
         self.parent.parent.updateInfoScreen(txt) # callback to Manager
         
         if direction == 'up':
-            self.moveField('down')
+            self.movePlayer('down')
         elif direction == 'down':
-            self.moveField('up')
+            self.movePlayer('up')
         elif direction == 'left':
-            self.moveField('right')
+            self.movePlayer('right')
         elif direction == 'right':
-            self.moveField('left')
+            self.movePlayer('left')
     
     def enterTown(self):
         print 'town discovered' 
         txt = 'entering the town...'
         self.parent.parent.updateInfoScreen(txt)
+        self.parent.current = 'town'
           
 class TownScreen(Screen):
     def __init__(self, **kwargs):
@@ -566,7 +398,7 @@ class Manager(BoxLayout):
             self.updateInfoScreen(txt)
         
         else:
-            self.screen.moveField(value.text)
+            self.screen.movePlayer(value.text)
             if value.text == 'move forward': self.location['y'] += 1
             elif value.text == 'move backward': self.location['y'] -= 1
             elif value.text == 'move right': self.location['x'] += 1
